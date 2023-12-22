f4604eb4af87e5b19a45692d9ea6403632afebf2
Toby Biggs <tbiggs2012@gmail.com>
2023-12-22 17:29:37 +0000

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<MaxWidthWrapper>
+			<div className="">Hello World</div>
+		</MaxWidthWrapper>
+	);
+}
